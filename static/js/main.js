1e35073ec111f79a54edf1cd13c485de10bf9817
avalonsky <gakiyukr@proton.me>
2022-11-30 08:21:55 +0800

    Update

--- static/js/main.js
+++ static/js/main.js
@@ -5,11 +5,6 @@ window.addEventListener("load", () => {
         img[i].setAttribute("draggable", "false");
     }
 
-    document.querySelector("[cn] [pay-btn]").addEventListener("click", () => {
-        imgWindow.innerHTML = `<img src="https://s3-jp-ap-3.040407.xyz/oss/pay/alwxcnqr.png" height="500" draggable="false">`;
-        imgWindow.style.display = "flex";
-    });
-
     document.querySelector("[hk] [pay-btn]").addEventListener("click", () => {
         imgWindow.innerHTML = `<img src="https://s3-jp-ap-3.040407.xyz/oss/pay/alwxhkqr.png" height="500" draggable="false">`;
         imgWindow.style.display = "flex";
@@ -20,10 +15,6 @@ window.addEventListener("load", () => {
         imgWindow.style.display = "flex";
     });
 
-    document.querySelector("[ECNY] [pay-btn]").addEventListener("click", () => {
-        imgWindow.innerHTML = `<img src="https://s3-jp-ap-3.040407.xyz/oss/pay/ecnypay.jpg" height="500" draggable="false">`;
-        imgWindow.style.display = "flex";
-    });
 
     document.querySelector("[USDT] [pay-btn]").addEventListener("click", () => {
         imgWindow.innerHTML = `<img src="https://s3-jp-ap-3.040407.xyz/oss/pay/usdtpay.png" height="500" draggable="false">`;
